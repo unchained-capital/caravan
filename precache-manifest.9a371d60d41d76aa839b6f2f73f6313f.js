0c17b72d3c0002adde39d320e205001344747ac6
Chris Howe <chris@howeville.com>
2019-11-12 12:17:07 -0600

    Updates

---
+++ precache-manifest.9a371d60d41d76aa839b6f2f73f6313f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "066bf394f548035e07b3",
+    "url": "/caravan/static/css/main.13d062d0.chunk.css"
+  },
+  {
+    "revision": "066bf394f548035e07b3",
+    "url": "/caravan/static/js/main.3fea18d1.chunk.js"
+  },
+  {
+    "revision": "792d9c5b5a485ad633ca",
+    "url": "/caravan/static/js/runtime~main.731ca210.js"
+  },
+  {
+    "revision": "c7081c6d12e8b9084273",
+    "url": "/caravan/static/css/2.1fb59395.chunk.css"
+  },
+  {
+    "revision": "c7081c6d12e8b9084273",
+    "url": "/caravan/static/js/2.267b965e.chunk.js"
+  },
+  {
+    "revision": "66481e66e2bea46e7b7d73df9d4953d9",
+    "url": "/caravan/static/media/logo.66481e66.png"
+  },
+  {
+    "revision": "6883865ece87696b0bbe2014a5a3a4f7",
+    "url": "/caravan/index.html"
+  }
+];
