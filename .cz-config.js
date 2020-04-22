81f8d1fe3b40f3daf09b5cdbb998bad7802f69e9
Walden Raines <walden@unchained-capital.com>
2020-04-22 10:43:09 -0400

    chore(other): adds commitlint and commitzen
    Add commitlint and commitzen to the project to promote and enforce
    conventional comments.
    
    https://conventionalcommits.org

---
+++ .cz-config.js
@@ -0,0 +1,59 @@
+const types = [
+  ` feat:     A new feature`,
+  ` fix:      A bug fix`,
+  ` docs:     Documentation only changes`,
+  ` style:    Changes that do not affect the meaning of the code
+             (white-space, formatting, missing semi-colons, etc)`,
+  ` refactor: A code change that neither fixes a bug nor adds a feature`,
+  ` perf:     A code change that improves performance`,
+  ` test:     Adding missing tests`,
+  ` revert:   Revert to a commit`,
+  ` WIP:      Work in progress`,
+  ` chore:    Changes to the build process or auxiliary tools
+             and libraries such as documentation generation`
+];
+
+const messages = {
+  type: `Select the type of change that you're committing:`,
+
+  subject: `Write a SHORT, IMPERATIVE tense description of the change:\n`,
+
+  body: `Provide a LONGER description of the change (optional). Use "|" to break new line:\n`,
+
+  breaking: `Describe any BREAKING CHANGES (optional):\n`,
+
+  footer: ` List any RELATED ISSUES to this change (optional).\n`,
+
+  confirmCommit: `Are you sure you want to proceed with the commit above?`
+};
+
+const scopes = [
+  "card",
+  "clientpicker",
+  "createaddress",
+  "hermit",
+  "slices",
+  "testrunner",
+  "wallet",
+  "other",
+];
+
+module.exports = {
+  types: types.map((type) => ({
+    value: type.split(":")[0].trim(),
+    name: type
+  })),
+
+  scopes: scopes.map((scope) => ({
+    name: scope
+  })),
+
+  messages,
+
+  subjectLimit: 65,
+  allowCustomScopes: false,
+  allowTicketNumber: false,
+  allowBreakingChanges: ["feat", "fix"],
+  breakingPrefix: "BREAKING CHANGE:",
+  footerPrefix: "ISSUES:"
+};
