a7ee2ae98dbaf877ffa73c16293d2345ede27e43
Buck Perley <bucko.perley@gmail.com>
2020-04-30 16:20:47 -0500

    isChange utility for testing if a path is a change path

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,7 @@
-/* eslint-disable import/prefer-default-export */
 import BigNumber from "bignumber.js";
 import { satoshisToBitcoins } from "unchained-bitcoin";
 
 export const DUST_IN_BTC = satoshisToBitcoins(BigNumber(546));
+export const DEFAULT_PREFIX = "m";
+export const CHANGE_INDEX = "/1";
+export const DEFAULT_CHANGE_PREFIX = DEFAULT_PREFIX + CHANGE_INDEX;
