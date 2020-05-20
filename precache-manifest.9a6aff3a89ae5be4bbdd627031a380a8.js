4b6c813c73e1dc2a05c139f72025c4b75bb63408
Buck Perley <bucko.perley@gmail.com>
2020-05-20 09:23:55 -0500

    Updates

---
+++ precache-manifest.9a6aff3a89ae5be4bbdd627031a380a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcac02ef84904f5565d15295e0dbe580",
+    "url": "/caravan/index.html"
+  },
+  {
+    "revision": "fb54b1bc04400fecad4f",
+    "url": "/caravan/static/css/2.d694ff75.chunk.css"
+  },
+  {
+    "revision": "6e8df5b29122a0e40f72",
+    "url": "/caravan/static/css/main.a7b7f3be.chunk.css"
+  },
+  {
+    "revision": "fb54b1bc04400fecad4f",
+    "url": "/caravan/static/js/2.cc7115ed.chunk.js"
+  },
+  {
+    "revision": "14dc2d5d9c21f9adae14e65c1a5fd9e6",
+    "url": "/caravan/static/js/2.cc7115ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e8df5b29122a0e40f72",
+    "url": "/caravan/static/js/main.afa7f4a6.chunk.js"
+  },
+  {
+    "revision": "c0d23c29dff07aa0630e",
+    "url": "/caravan/static/js/runtime-main.26c5e46f.js"
+  },
+  {
+    "revision": "66481e66e2bea46e7b7d73df9d4953d9",
+    "url": "/caravan/static/media/logo.66481e66.png"
+  }
+]);
