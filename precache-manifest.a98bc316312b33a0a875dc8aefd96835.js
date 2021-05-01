d12ab715d3efb86e7014ee98fe1ae7fd753928ed
TravisCI <Travis CIci@unchained-capital.com>
2021-05-01 01:36:03 +0000

    Updates

---
+++ precache-manifest.a98bc316312b33a0a875dc8aefd96835.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a66569c14adbd9e499e63ce3c253836",
+    "url": "/caravan/index.html"
+  },
+  {
+    "revision": "6e41fa6c7f1becc26c6d",
+    "url": "/caravan/static/css/2.f2850afe.chunk.css"
+  },
+  {
+    "revision": "58ecfcb2662d6f9e5e92",
+    "url": "/caravan/static/css/main.a26c6f63.chunk.css"
+  },
+  {
+    "revision": "6e41fa6c7f1becc26c6d",
+    "url": "/caravan/static/js/2.17dd0e6e.chunk.js"
+  },
+  {
+    "revision": "0b52b5e0beb68b4f3ea9dd25d3a4440b",
+    "url": "/caravan/static/js/2.17dd0e6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58ecfcb2662d6f9e5e92",
+    "url": "/caravan/static/js/main.9a5cde10.chunk.js"
+  },
+  {
+    "revision": "463b8ec66a3715f618ea",
+    "url": "/caravan/static/js/runtime-main.7ddb6876.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/caravan/precache-manifest.84ccd275765324002cdfffb20d694d06.js"
+  "/caravan/precache-manifest.a98bc316312b33a0a875dc8aefd96835.js"
 );
 
 self.addEventListener('message', (event) => {
