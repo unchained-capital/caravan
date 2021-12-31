4b085bc78e7a81ee7540d5203b7caa689f0cd93c
Chris Howe <chris@unchained-capital.com>
2021-12-30 18:47:52 -0600

    Revert "Updates"
    This reverts commit 12c4888c8b2408a505c033c363324596907238c0.

---
+++ precache-manifest.3ff44b46f6b8dcde2f3b9377f8c025c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbd08d877144e6f10dc12a7929b3b8d3",
+    "url": "/caravan/index.html"
+  },
+  {
+    "revision": "220fa3961f2b0a8eb49c",
+    "url": "/caravan/static/css/2.f2850afe.chunk.css"
+  },
+  {
+    "revision": "da5b46334994a7873417",
+    "url": "/caravan/static/css/main.a26c6f63.chunk.css"
+  },
+  {
+    "revision": "220fa3961f2b0a8eb49c",
+    "url": "/caravan/static/js/2.fc8bec25.chunk.js"
+  },
+  {
+    "revision": "25a69d482e4bdd8ec9195ba51047d5d3",
+    "url": "/caravan/static/js/2.fc8bec25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da5b46334994a7873417",
+    "url": "/caravan/static/js/main.bbba2335.chunk.js"
+  },
+  {
+    "revision": "463b8ec66a3715f618ea",
+    "url": "/caravan/static/js/runtime-main.7ddb6876.js"
+  }
+]);
