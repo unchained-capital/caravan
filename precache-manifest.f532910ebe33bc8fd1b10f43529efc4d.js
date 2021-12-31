ee93d9956aefbba5dfac94b80f4f132e298721da
Chris Howe <chris@unchained-capital.com>
2021-12-30 18:48:10 -0600

    Revert "Updates"
    This reverts commit 8aa8cc77c8aba1f36ebd4528804bdbcac95c59c0.

--- precache-manifest.f532910ebe33bc8fd1b10f43529efc4d.js
+++ precache-manifest.f532910ebe33bc8fd1b10f43529efc4d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9c2f725907e107581e15c3161cc17d4f",
+    "revision": "6a240f887ff725462a614bfde561a583",
     "url": "/caravan/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/caravan/static/css/2.f2850afe.chunk.css"
   },
   {
-    "revision": "3432b9ba5ec5cb8d954c",
+    "revision": "d769503e5c84bc3a7b63",
     "url": "/caravan/static/css/main.a26c6f63.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/caravan/static/js/2.67a9ef80.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3432b9ba5ec5cb8d954c",
-    "url": "/caravan/static/js/main.420ad095.chunk.js"
+    "revision": "d769503e5c84bc3a7b63",
+    "url": "/caravan/static/js/main.7319e44e.chunk.js"
   },
   {
     "revision": "463b8ec66a3715f618ea",
