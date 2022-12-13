8ec051b0db507fc95fbd3d0d37fac0dac9543ed0
Justin Moore <11jmoore@pm.me>
2022-12-12 23:27:24 -0800

    refactor(scriptexplorer): finish removal of psbt link

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,7 +15,6 @@ import Wallet from "./Wallet";
 import CreateAddress from "./CreateAddress";
 import TestSuiteRun from "./TestSuiteRun";
 import ScriptExplorer from "./ScriptExplorer";
-import PsbtSpend from "./Psbt";
 import HermitPsbtInterface from "./Hermit/HermitPsbtInterface";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
@@ -35,7 +34,6 @@ const App = () => (
               <Route path="/address" component={CreateAddress} />
               <Redirect from="/spend" to="/script" />
               <Route path="/script" component={ScriptExplorer} />
-              <Route path="/psbt" component={PsbtSpend} />
               <Route path="/wallet" component={Wallet} />
               <Route path="/hermit-psbt" component={HermitPsbtInterface} />
               <Route path="/help" component={Help} />
