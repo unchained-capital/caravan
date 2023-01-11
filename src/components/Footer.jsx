05c3e549fce6a304f967cdfecb4937a1f2349a15
Justin Moore <11jmoore@pm.me>
2023-01-11 03:33:27 -0800

    chore(hermit): add date to footer yurt canary

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -28,7 +28,7 @@ const Footer = () => (
         <p>
           Copyright&nbsp;
           {new Date(Date.now()).getFullYear()} by Unchained Capital and released
-          under an MIT license. 🛖
+          under an MIT license. 🛖20230111
         </p>
       </Grid>
 
