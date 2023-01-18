8c0c130548398dd122bbb0d038a0ea7eff29dda6
Justin Moore <11jmoore@pm.me>
2023-01-17 21:50:06 -0800

    chore(wallet): remove a footer canary

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -28,7 +28,7 @@ const Footer = () => (
         <p>
           Copyright&nbsp;
           {new Date(Date.now()).getFullYear()} by Unchained Capital and released
-          under an MIT license. 🛖20230111
+          under an MIT license.
         </p>
       </Grid>
 
