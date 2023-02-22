9030f441cf50438560dca5b946cd6fe4e83447ce
buck <buck@unchained-capital.com>
2023-02-21 18:56:46 -0600

    Ledger v2 test suite (#287)
    * fix: some build system fixes
    
    * feat: add tests for ledger address and policy checks
    
    * fix: update fixture api for consistent policies
    
    * feat: ledger v2 signing support in test suite
    
    * chore: some housekeeping

--- src/tests/ledger.js
+++ src/tests/ledger.js
@@ -3,7 +3,11 @@ import { LEDGER } from "unchained-wallets";
 import publicKeyTests from "./publicKeys";
 import extendedPublicKeyTests from "./extendedPublicKeys";
 import { signingTests } from "./signing";
+import addressTests from "./addresses";
+import registrationTests from "./registration";
 
 export default publicKeyTests(LEDGER)
   .concat(extendedPublicKeyTests(LEDGER))
-  .concat(signingTests(LEDGER));
+  .concat(signingTests(LEDGER))
+  .concat(addressTests(LEDGER))
+  .concat(registrationTests(LEDGER));
