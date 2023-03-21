f73dd19bee28e45dbfe98753442e386e3c15c4c3
Dylan Bathurst <dylanbathurst@gmail.com>
2023-03-20 17:19:52 -0700

    refactor(tests): configure jest and bable for running package tests and fixing a few broken tests

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfil
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "build",
+  },
   define: {
     VITE_APP_VERSION: JSON.stringify(process.env.npm_package_version),
     VITE_GIT_SHA: JSON.stringify(process.env.VITE_GIT_SHA),
