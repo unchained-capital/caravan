8bb54086ac0b502c8413d81b28a5d1fae3f4aceb
Dylan Bathurst <dylanbathurst@gmail.com>
2023-03-24 13:32:09 -0700

    fix(build): add base path to vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/caravan/",
   plugins: [
     react(),
     nodePolyfills({
