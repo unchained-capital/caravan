1d0f938e4fd0f49df30d064e3b0078f111d6acb1
winsby <tristanwindham@gmail.com>
2023-11-13 11:55:03 -0600

    fix: revert base routing

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/caravan/",
+  base: "/caravan/#",
   resolve: {
     alias: {
       utils: path.resolve(__dirname, "./src/utils"),
