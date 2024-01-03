607d9b8c6461d4762f0f5873c5a7045eba9a9e9f
buck <buck@unchained.com>
2024-01-03 14:44:13 -0600

    fix: utility accepts string instead of BN

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,7 @@
-import BigNumber from "bignumber.js";
 import { satoshisToBitcoins } from "unchained-bitcoin";
 
 export const DUST_IN_SATOSHIS = 546;
-export const DUST_IN_BTC = satoshisToBitcoins(BigNumber(DUST_IN_SATOSHIS));
+export const DUST_IN_BTC = satoshisToBitcoins(DUST_IN_SATOSHIS.toString());
 export const DEFAULT_PREFIX = "m";
 export const CHANGE_INDEX = "/1";
 export const DEFAULT_CHANGE_PREFIX = DEFAULT_PREFIX + CHANGE_INDEX;
